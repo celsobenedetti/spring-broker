a067f250cb0d4b8550c73aec2f374abf42598f46
Celso Benedetti <celsobenedetti2@gmail.com>
2023-04-24 22:03:22 -0300

    Initial commit

---
+++ src/main/java/djc/broker/CatshopBrokerApplication.java
@@ -0,0 +1,13 @@
+package djc.broker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatshopBrokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatshopBrokerApplication.class, args);
+	}
+
+}
